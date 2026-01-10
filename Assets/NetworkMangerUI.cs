0a95148f73e3ba0341eabcdec9d3633823bc970c
Ozder722 <carlosozdk@gmail.com>
2026-01-10 12:13:08 +0100

    setup af multiplayer

---
+++ Assets/NetworkMangerUI.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkMangerUI : MonoBehaviour
+{
+    [SerializeField] private Button serverBtn;
+    [SerializeField] private Button hostBtn;
+    [SerializeField] private Button clientBtn;
+
+
+
+
+}
