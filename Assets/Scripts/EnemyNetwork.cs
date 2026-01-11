9245e15586a1649bfe5f7e3fa6a0063d4b983d9d
Ozder722 <carlosozdk@gmail.com>
2026-01-11 10:45:57 +0100

    spawn aff objekter og enemy går på rute

---
+++ Assets/Scripts/EnemyNetwork.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNetwork : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private List<Transform> walkPoints;
+
+    private int currentWalkPointIndex = 0;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void Start()
+    {
+        if (walkPoints.Count > 0)
+        {
+            agent.SetDestination(walkPoints[0].position);
+        }
+    }
+
+    private void Update()
+    {
+        MoveAlongPath();
+    }
+
+    private void MoveAlongPath()
+    {
+        if (walkPoints.Count == 0) return;
+
+        if (!agent.pathPending && agent.remainingDistance <= 0.2f)
+        {
+            currentWalkPointIndex++;
+
+            if (currentWalkPointIndex >= walkPoints.Count)
+            {
+                ReachedEnd();
+                return;
+            }
+
+            agent.SetDestination(walkPoints[currentWalkPointIndex].position);
+        }
+    }
+
+    private void ReachedEnd()
+    {
+
+        Console.WriteLine("Nået til enden");
+        // TODO:
+        // - skade spilleren
+        // - reducér liv
+        // - despawn enemy
+
+        Destroy(gameObject);
+    }
+}
